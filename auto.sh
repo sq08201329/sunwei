5d6c9779c62677d57505ed6fd6c15e8e9a84b026
sunqi <sq08201329@126.com>
2016-10-03 12:46:42 +0800

    2016骞´10鏈03鏃¥

--- auto.sh
+++ auto.sh
@@ -1,6 +1,7 @@
 #cmd.exe /c "C:\Windows\System32\schtasks.exe /create /sc DAILY /st 12:00:00  /tn "sunqi01" /tr "D:\sunwei\sunwei\auto.sh""
 #C:\Users\Administrator>C:\Windows\System32\schtasks.exe /create /sc HOURLY /mo 12 /tn "sunqi01" /tr "D:\sunwei\sunwei\auto.sh"
-git add *
+git add .
+git status
 #cmd.exe /c "chcp 65001"
 dt=$(date "+%Y年%m月%d日")
 git commit -m "$dt"
